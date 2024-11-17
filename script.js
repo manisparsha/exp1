4d2f299ec94307fab8e42920831411276de496b7
manisparsha <sparsham308@gmail.com>
2024-11-17 22:37:32 +0530

    added console

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById('registration-form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert("Registration Successful!");
+    console.log("Form Submitted!");
 });
